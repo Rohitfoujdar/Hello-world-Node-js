ac70e18037d2664362fe3663f6fe0b8c2af973a0
Rohitfoujdar <rohitfoujdar8696@gmail.com>
2024-11-12 18:28:28 +0530

    person.js done

--- models/menu.js
+++ models/menu.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const menuItemSchema = new mongoose.Schema({
     name:{
@@ -31,8 +32,42 @@ const menuItemSchema = new mongoose.Schema({
     num_sales:{
        type:Number,
        default:0
+    },
+
+    username:{
+      type:String,
+      required:true
+    },
+   
+    password:{
+      type:String,
+      required: true
     }
    })
+
+   menuItemSchema.pre("save", async function(next){
+      const menuItem = this;
+      if(!menuItem.isModified("password")) return next();
+    try{
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(menuItem.password, salt);
+       menuItem.password = hashedPassword;
+       next();
+    }
+    catch(error){
+       return next(error)
+    }
+ })
+ 
+ menuItemSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+       const isMatch = await bcrypt.compare(candidatePassword, this.password);
+       return isMatch;
+    }catch(error){
+       throw error;
+    }
+ }
+
    
    const menuItem = mongoose.model("menu",menuItemSchema);
    module.exports = menuItem;
